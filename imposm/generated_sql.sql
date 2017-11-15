6e5c800f94ae42942bff4adbf6c72d374b8d61e1
nlehuby <n.lehuby@qwant.com>
2017-11-15 11:28:33 +0100

    Building only in z14

--- imposm/generated_sql.sql
+++ imposm/generated_sql.sql
@@ -1333,7 +1333,7 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, ramp int, oneway int
     ORDER BY z_order ASC;
 $$ LANGUAGE SQL IMMUTABLE;
 DO $$ BEGIN RAISE NOTICE 'Layer building'; END$$;-- etldoc: layer_building[shape=record fillcolor=lightpink, style="rounded,filled",
--- etldoc:     label="layer_building | <z13> z13 | <z14_> z14+ " ] ;
+-- etldoc:     label="layer_building | <z14_> z14+ " ] ;
 
 CREATE OR REPLACE FUNCTION as_numeric(text) RETURNS NUMERIC AS $$
  -- Inspired by http://stackoverflow.com/questions/16195986/isnumeric-with-postgresql/16206123#16206123
@@ -1409,13 +1409,6 @@ CREATE OR REPLACE FUNCTION layer_building(bbox geometry, zoom_level int)
 RETURNS TABLE(geometry geometry, osm_id bigint, render_height int, render_min_height int) AS $$
     SELECT geometry, osm_id, render_height, render_min_height
     FROM (
-        -- etldoc: osm_building_polygon_gen1 -> layer_building:z13
-        SELECT
-            osm_id, geometry,
-            NULL::int AS render_height, NULL::int AS render_min_height
-        FROM osm_building_polygon_gen1
-        WHERE zoom_level = 13 AND geometry && bbox
-        UNION ALL
         -- etldoc: osm_building_polygon -> layer_building:z14_
         SELECT DISTINCT ON (osm_id)
            osm_id, geometry,
