f911d3a62e69a9f18e76ae890f21452499070f57
Adrien Matissart <a.matissart@qwantresearch.com>
2018-06-05 12:35:56 +0200

    Add global_id in poi tiles

--- imposm/generated_poi.sql
+++ imposm/generated_poi.sql
@@ -270,12 +270,25 @@ CREATE MATERIALIZED VIEW osm_poi_stop_rank AS (
 
 ALTER TABLE osm_poi_point ADD COLUMN IF NOT EXISTS agg_stop INTEGER DEFAULT NULL;
 SELECT update_osm_poi_point_agg();
+CREATE OR REPLACE FUNCTION global_id_from_imposm(osm_id bigint, osm_type text)
+RETURNS TEXT AS $$
+    SELECT CONCAT(
+        'osm:',
+        CASE WHEN osm_id<0
+            THEN CONCAT('relation:', -osm_id)
+            ELSE CONCAT(osm_type, ':', osm_id)
+        END
+    );
+$$ LANGUAGE SQL IMMUTABLE;
+
+
 -- etldoc: layer_poi[shape=record fillcolor=lightpink, style="rounded,filled",
 -- etldoc:     label="layer_poi | <z12> z12 | <z13> z13 | <z14_> z14+" ] ;
 
 CREATE OR REPLACE FUNCTION layer_poi(bbox geometry, zoom_level integer, pixel_width numeric)
-RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, name_de text, tags hstore, class text, subclass text, agg_stop integer, "rank" int) AS $$
-    SELECT osm_id_hash AS osm_id, geometry, NULLIF(name, '') AS name,
+RETURNS TABLE(osm_id bigint, global_id text, geometry geometry, name text, name_en text, name_de text, tags hstore, class text, subclass text, agg_stop integer, "rank" int) AS $$
+    SELECT osm_id_hash AS osm_id, global_id,
+        geometry, NULLIF(name, '') AS name,
         COALESCE(NULLIF(name_en, ''), name) AS name_en,
         COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
         tags,
@@ -294,7 +307,9 @@ RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, name_de
         -- etldoc: osm_poi_point ->  layer_poi:z12
         -- etldoc: osm_poi_point ->  layer_poi:z13
         SELECT *,
-            osm_id*10 AS osm_id_hash FROM osm_poi_point
+            osm_id*10 AS osm_id_hash,
+            global_id_from_imposm(osm_id, 'node') as global_id
+        FROM osm_poi_point
             WHERE geometry && bbox
                 AND zoom_level BETWEEN 12 AND 13
                 AND ((subclass='station' AND mapping_key = 'railway')
@@ -303,7 +318,9 @@ RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, name_de
 
         -- etldoc: osm_poi_point ->  layer_poi:z14_
         SELECT *,
-            osm_id*10 AS osm_id_hash FROM osm_poi_point
+            osm_id*10 AS osm_id_hash,
+            global_id_from_imposm(osm_id, 'node') as global_id
+        FROM osm_poi_point
             WHERE geometry && bbox
                 AND zoom_level >= 14
 
@@ -314,7 +331,8 @@ RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, name_de
             NULL::INTEGER AS agg_stop,
             CASE WHEN osm_id<0 THEN -osm_id*10+4
                 ELSE osm_id*10+1
-            END AS osm_id_hash
+            END AS osm_id_hash,
+            global_id_from_imposm(osm_id, 'way') as global_id
         FROM osm_poi_polygon
             WHERE geometry && bbox
                 AND zoom_level BETWEEN 12 AND 13
@@ -327,7 +345,8 @@ RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, name_de
             NULL::INTEGER AS agg_stop,
             CASE WHEN osm_id<0 THEN -osm_id*10+4
                 ELSE osm_id*10+1
-            END AS osm_id_hash
+            END AS osm_id_hash,
+            global_id_from_imposm(osm_id, 'way') as global_id
         FROM osm_poi_polygon
             WHERE geometry && bbox
                 AND zoom_level >= 14
